7c7ee8b9ee8408ba88bc66a0ec0798ffc7fbda02
shane tully <esc201@gmail.com>
2012-01-04 13:45:41 -0500

    completed UI and textmode

---
+++ SharedUtils.py
@@ -0,0 +1,18 @@
+# Even though MySQLdb performs input sanitization internally, 
+# it doesn't hurt to do it oursevles just to be on the safe side
+def sanitizeInput(input):
+   # Keep a copy of the possibly mixed-case input
+   origInput = input
+   input.upper()
+
+   # The reserved words to check for
+   # There are many more, of course, but these should thwart the most dangerous attacks
+   keywords = ["DELETE", "UPDATE", "DROP", "CREATE", "SELECT", "INSERT", "ALTER"]
+
+   # Check for a match
+   for i in keywords:
+      if i in input:
+         return ""
+   
+   # If no match, return the original input
+   return origInput
